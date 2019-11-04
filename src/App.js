46ed2402150609ba28c68da4477a24650202a6f6
rickard-k2 <rkarlsson320@gmail.com>
2019-11-04 13:11:10 +0100

    Fetch categories and questions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [categories, setCategories] = useState(null);
+  const [numQ, setNumQ] = useState("");
+  const [selectedCategory, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("easy");
+  const [questions, setQuestions] = useState(null);
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api_category.php")
+      .then(res => res.json())
+      .then(data => {
+        setCategories(data.trivia_categories);
+      });
+  }, []);
+
+  function handleSelect(e) {
+    setCategory(e.target.value);
+  }
+
+  function handleNumber(e) {
+    setNumQ(e.target.value);
+  }
+
+  function handleDifficulty(e) {
+    setDifficulty(e.target.value);
+  }
+
+  function getQuestions() {
+    fetch(
+      `https://opentdb.com/api.php?amount=${numQ}&category=${selectedCategory}&difficulty=${difficulty}&type=multiple&encode=base64`
+    )
+      .then(res => res.json())
+      .then(data => {
+        setQuestions(JSON.stringify(data, null, 4));
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Select category: </label>
+      <select
+        placeholder={"Category"}
+        onChange={handleSelect}
+        value={selectedCategory}
+      >
+        <option value="" disabled selected>
+          - - - - - -
+        </option>
+        {categories
+          ? categories.map(category => {
+              return (
+                <option key={category.name} value={category.id}>
+                  {category.name}
+                </option>
+              );
+            })
+          : null}
+      </select>
+      <label>Number of Questions: </label>
+      <input onChange={handleNumber} value={numQ} type="number"></input>
+      <label>Difficulty: </label>
+      <select onChange={handleDifficulty}>
+        <option key="easy" value="easy">
+          Easy
+        </option>
+        <option key="medium" value="medium">
+          Medium
+        </option>
+        <option key="hard" value="hard">
+          Hard
+        </option>
+      </select>
+      <button onClick={getQuestions}>-- GO! --</button>
+      {questions}
     </div>
   );
 }
